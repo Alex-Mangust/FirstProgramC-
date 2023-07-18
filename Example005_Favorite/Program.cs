2d216444884c7d4e8df8df5f2c7796e2be59f58b
Alex Mangust <sanya.stogny@gmail.com>
2023-07-19 03:15:27 +0800

    add Example005_Favorite

---
+++ Example005_Favorite/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+if(name == "Саша")
+{
+    Console.Write("Ура, это же " + name + "!");
+}
+else
+{
+    Console.Write("Привет " + name);
+}
