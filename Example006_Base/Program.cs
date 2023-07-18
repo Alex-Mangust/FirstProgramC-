5e66757ce5ebe6d0cd97b5f427dafc1571b7396d
Alex Mangust <sanya.stogny@gmail.com>
2023-07-19 03:40:25 +0800

    add Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 1, b = 2, c = 6, d = 8, e = 4;
+int max = a;
+int number = 0;
+while(number < 5)
+{
+    if(max < b)
+    {
+        max = b;
+    }
+    else if(max < c)
+    {
+        max = c;
+    }
+    else if(max < d)
+    {
+        max = d;
+    }
+    else if(max < e)
+    {
+        max = e;
+    }
+    number++;
+}
+Console.WriteLine("max = " + max);
