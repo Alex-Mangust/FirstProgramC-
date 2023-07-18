0b2ce3f25b666bb7a84b4c7454ef2d761ebfda11
Alex Mangust <sanya.stogny@gmail.com>
2023-07-19 02:55:36 +0800

    add random

--- Examper003_Sum_A_Two_Counter/Program.cs
+++ Examper003_Sum_A_Two_Counter/Program.cs
@@ -8,8 +8,10 @@ int sum = count_first + count_second;
 Console.Write("Сумма чисел равна ");
 Console.WriteLine(sum); */
 
-int count_first = 3;
-int count_second = 4;
+int count_first = new Random().Next(1, 10);
+int count_second = new Random().Next(1, 10);
+Console.WriteLine(count_first);
+Console.WriteLine(count_second);
 int sum = count_first + count_second;
 Console.Write("Сумма чисел равна ");
 Console.WriteLine(sum);
