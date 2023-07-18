1780c915da2c3f76a47b046eca133b695ef986a2
Alex Mangust <sanya.stogny@gmail.com>
2023-07-19 02:40:49 +0800

    add Example003_Sum

---
+++ Examper003_Sum_A_Two_Counter/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Console.Write("Введите первое число: ");
+Console.Read();
+int count_first = Console.Read();
+Console.Write("Введите второе число: ");
+Console.Read();
+int count_second = Console.Read();
+int sum = count_first + count_second;
+Console.Write("Сумма чисел равна ");
+Console.WriteLine(sum); */
+
+int count_first = 3;
+int count_second = 4;
+int sum = count_first + count_second;
+Console.Write("Сумма чисел равна ");
+Console.WriteLine(sum);
